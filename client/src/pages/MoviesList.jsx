e00016b0d86955bde5ce78a34c7119a40158a243
Blake Han <han.sangyeup@gmail.com>
2021-07-04 14:55:26 -0400

    Add pages to index file

---
+++ client/src/pages/MoviesList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const MoviesList = () => (
+  <div>
+    <p>In this page you'll see the list of movies</p>
+  </div>
+);
+
+export default MoviesList;
---
+++ client/src/pages/MoviesUpdate.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const MoviesUpdate = () => (
+  <div>
+    <p>In this page you'll see the form to update the movies</p>
+  </div>
+);
+
+export default MoviesUpdate;
