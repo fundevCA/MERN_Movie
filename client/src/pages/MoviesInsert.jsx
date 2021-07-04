9aee5876a481c25e2e120de58ee4602fc93e5a58
Blake Han <han.sangyeup@gmail.com>
2021-07-04 14:53:57 -0400

    Create Movies components

---
+++ client/src/pages/MoviesInsert.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const MoviesInsert = () => (
+  <div>
+    <p>In this page you'll see the form to add a movies</p>
+  </div>
+);
+
+export default MoviesInsert;
